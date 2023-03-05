5a907322b8ed5e83b97f2e871e8c6ed2cea8c46b
lukas-braziu <lukas.braziulis@gmail.com>
2023-03-05 08:16:18 +0200

    watchlist-css

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <table>
+    <table class="watchlist-table">
         <th>Movie Title</th>
         <th>Release Year</th>
         <th>Director</th>
@@ -10,11 +10,11 @@
 
         @foreach($watchlists as $watchlist)
         <tr>
-            <td><a href="{{route('movies.show', $watchlist->id)}}">{{$watchlist->title}}</a></td>
+            <td><a href="{{route('movies.show', $watchlist->id)}}" class="color-white underline">{{$watchlist->title}}</a></td>
             <td>{{$watchlist->release_year}} </td>
             <td>{{$watchlist->director_name}} {{$watchlist->director_last_name}}</td>
             <td>{{$watchlist->rating}} / 10</td>
-            <td><a href="{{route('watchlist.delete', $watchlist->id)}}">Remove</a></td>
+            <td><a href="{{route('watchlist.delete', $watchlist->id)}}" class="color-white underline">Remove</a></td>
         </tr>
         @endforeach
     </table>
